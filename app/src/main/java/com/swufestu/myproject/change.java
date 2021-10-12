81545626cb92e6fdb7d23342cb933cc031fdfc34
likeni <249495196@qq.com>
2021-10-12 20:04:26 +0800

    10.12更新

--- app/src/main/java/com/swufestu/myproject/change.java
+++ app/src/main/java/com/swufestu/myproject/change.java
@@ -5,42 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class change extends AppCompatActivity {
-    double change1 = 0;
-    double change2 = 0;
-    double change3 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
-        Intent intent = getIntent();
-        change1 = intent.getDoubleExtra("change1", 12.3);
-        change2 = intent.getDoubleExtra("change2", 12.3);
-        change3 = intent.getDoubleExtra("change3", 12.3);
-        EditText text1 = findViewById(R.id.editTextNumberDecimal2);
-        text1.setText("before:"+change1);
-        EditText text2 = findViewById(R.id.editTextNumberDecimal3);
-        text2.setText("before:"+change2);
-        EditText text3 = findViewById(R.id.editTextNumberDecimal4);
-        text3.setText("before:"+change3);
+
+        final Bundle bundle=getIntent().getExtras();    //接收Extras
+        float ex_rate1 = bundle.getFloat("ex_rate1");
+        float ex_rate2 = bundle.getFloat("ex_rate2");
+        float ex_rate3 = bundle.getFloat("ex_rate3");
+
+        EditText rate1 = (EditText) findViewById(R.id.ex_rate1);
+        rate1.setText(""+ex_rate1);
+        EditText rate2 = (EditText) findViewById(R.id.ex_rate2);
+        rate2.setText(""+ex_rate2);
+        EditText rate3 = (EditText) findViewById(R.id.ex_rate3);
+        rate3.setText(""+ex_rate3);
+
+        Button button2=(Button)findViewById(R.id.save);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float new_rate1 = Float.valueOf(rate1.getText().toString());
+                float new_rate2 = Float.valueOf(rate2.getText().toString());
+                float new_rate3 = Float.valueOf(rate3.getText().toString());
+
+                Intent intent1 = getIntent();//构造Intent，用于传递数据
+                Bundle bdl = new Bundle();
+                bdl.putFloat("new_rate1",new_rate1);
+                bdl.putFloat("new_rate2",new_rate2);
+                bdl.putFloat("new_rate3",new_rate3);
+                intent1.putExtras(bdl);
+                setResult(RESULT_OK,intent1);//专门向上一个活动传递数据的
+                finish();
+            }
+        });
     }
-    public void onclick(View view)
-    {
-//        Intent intent = new Intent(this, MainActivity.class);
-        Intent intent = getIntent();
-        EditText text1 = findViewById(R.id.editTextNumberDecimal2);
-        EditText text2 = findViewById(R.id.editTextNumberDecimal3);
-        EditText text3 = findViewById(R.id.editTextNumberDecimal4);
-        double changed1 = Double.parseDouble(text1.getText().toString());
-        double changed2 = Double.parseDouble(text2.getText().toString());
-        double changed3 = Double.parseDouble(text3.getText().toString());
-        intent.putExtra("changed1", changed1);
-        intent.putExtra("changed2", changed2);
-        intent.putExtra("changed3", changed3);
-        setResult(3, intent);
-        finish();
-    }
-    }
+}
--- app/src/main/java/com/swufestu/myproject/show.java
+++ app/src/main/java/com/swufestu/myproject/show.java
@@ -1,61 +1,216 @@
 package com.swufestu.myproject;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+//import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class show extends AppCompatActivity {
-    double money = 0;
-    double change1 = 0.15;
-    double change2 = 6.8;
-    double change3 = 7.5;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class show extends AppCompatActivity implements Runnable{
+
+    private static final String TAG = "show";
+    MyHandler handler = new MyHandler(this);
+    public String record_date;
+    public float ex_rate1 = -1;
+    public float ex_rate2 = -1;
+    public float ex_rate3 = -1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
-    }
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 1 && resultCode == 3)
-        {
-            change1 = data.getDoubleExtra("changed1", 0.15);
-            change2 = data.getDoubleExtra("changed2", 6.8);
-            change3 = data.getDoubleExtra("changed3", 7.5);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        Date currentTime = Calendar.getInstance().getTime();
+        String actual_date = formatter.format(currentTime);
+        Log.i(TAG, "onCreate: actual date=" + actual_date);
+
+        SharedPreferences sp = getSharedPreferences("my_rate_1",show.MODE_PRIVATE);
+        PreferenceManager.getDefaultSharedPreferences(this);
+        ex_rate1 = sp.getFloat("ex_rate_1",0.1547f);
+        ex_rate2 = sp.getFloat("ex_rate_2",0.132f);
+        ex_rate3 = sp.getFloat("ex_rate_3",182.4343f);
+        record_date = sp.getString("record_date","20210928");
+
+        // to compare actual_date with record_date
+        Log.i(TAG, "onCreate: record=" + Long.parseLong(record_date));
+        Log.i(TAG, "onCreate: actual=" + Long.parseLong(actual_date));
+        if(Long.parseLong(record_date) < Long.parseLong(actual_date)){
+            Thread t = new Thread(this);
+            t.start();
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("record_date", actual_date);
+            editor.apply();
+            Log.i(TAG, "create: need to update!\nThe exchange rate has been updated! time: " + actual_date);
         }
+
+
+
+
+        //exchange dollar
+        Button ex1 = (Button) findViewById(R.id.ex_1);
+        ex1.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            exchange(ex_rate1,"美元");
+        });
+        //exchange euro
+        Button ex2 = (Button) findViewById(R.id.ex_2);
+        ex2.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            exchange(ex_rate2,"欧元");
+        });
+        //exchange won
+        Button ex3 = (Button) findViewById(R.id.ex_3);
+        ex3.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            exchange(ex_rate3,"韩元");
+        });
+        //config exchange rate
+        Button config = (Button) findViewById(R.id.config);
+        config.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            Intent config_interface = new Intent(show.this,change.class);
+            config_interface.putExtra("ex_rate1",ex_rate1);
+            config_interface.putExtra("ex_rate2",ex_rate2);
+            config_interface.putExtra("ex_rate3",ex_rate3);
+            startActivityForResult(config_interface,1);
+        });
+
+    }
+
+
+
+    void exchange(double ex_rate,String s){
+        EditText yuan_input = (EditText) findViewById(R.id.yuan_input);
+        if(yuan_input.getText().toString().length() == 0){
+            Toast.makeText(show.this, "No valid data entered!Please retry!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        TextView result = (TextView)findViewById(R.id.result);
+        float yuan = Float.parseFloat(yuan_input.getText().toString());
+        result.setText(yuan+"人民币≈"+String.format("%.2f",ex_rate*yuan)+s);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    // config update
+                    final Bundle bdl = data.getExtras();
+                    ex_rate1 = bdl.getFloat("new_rate1");
+                    Toast.makeText(show.this,String.valueOf(ex_rate1), Toast.LENGTH_SHORT).show();
+                    ex_rate2 = bdl.getFloat("new_rate2");
+                    ex_rate3 = bdl.getFloat("new_rate3");
+                    SharedPreferences sp = getSharedPreferences("my_rate_1",show.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putFloat("ex_rate_1",ex_rate1);
+                    editor.putFloat("ex_rate_2",ex_rate2);
+                    editor.putFloat("ex_rate_3",ex_rate3);
+
+                    editor.apply();
+                }
+                break;
+            default:
+        }
     }
 
-    public void onCLickB(View view)
-    {
-        EditText text = findViewById(R.id.editTextNumberDecimal);
-        money = Double.parseDouble(text.getText().toString());
-        if(view.getId() == R.id.button)
-        {
-            money = money * change1;
+    @Override
+    public void run() {
+        Message msg = handler.obtainMessage(5);
+        // get information from internet
+//        URL url = null;
+//        try{
+//            url = new URL("https://www.usd-cny.com/bankofchina.htm");
+//            HttpsURLConnection http  = (HttpsURLConnection) url.openConnection();
+//            InputStream in = http.getInputStream();
+//            String html = Stream2String(in);
+//            Log.i(TAG, "run: "+html);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        msg.obj = "hello from run()";
+//        handler.sendMessage(msg);
+
+        // get information from internet by jsoup
+        try {
+            Document doc = Jsoup.connect("https://www.usd-cny.com/bankofchina.htm").get();
+            Elements rate_info  = doc.select("tr:matches(美元|欧元|韩元)");
+            for(Element info:rate_info){
+                Elements tds = info.getElementsByTag("td");
+                String Currency = tds.get(0).text();
+                float ex_rate = 100/Float.parseFloat(tds.get(5).text());
+                Log.i(TAG, "run: Currency = " + Currency + "\tex_rate = " + (ex_rate));
+                //进行汇率更新
+                SharedPreferences sp = getSharedPreferences("my_rate_1",show.MODE_PRIVATE);
+                @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sp.edit();
+                switch (Currency){
+                    case "美元":
+                        ex_rate1 = ex_rate;
+                        editor.putFloat("ex_rate_1",ex_rate);
+                        continue;
+                    case "欧元":
+                        ex_rate2 = ex_rate;
+                        editor.putFloat("ex_rate_2",ex_rate);
+                        continue;
+                    case "韩元":
+                        ex_rate3 = ex_rate;
+                        editor.putFloat("ex_rate_3",ex_rate);
+                }
+                editor.apply();
+//                Log.i(TAG, "run: The exchange rate has been updated!");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else if(view.getId() == R.id.button2)
-        {
-            money = money * change2;
-        }
-        else if(view.getId() == R.id.button3)
-        {
-            money = money * change3;
-        }
-        TextView views = findViewById(R.id.textView2);
-        views.setText("changed:"+String.format("%.2f",money));
+        msg.obj = "hello from run()";
+        handler.sendMessage(msg);
     }
 
-    public  void onClickC(View view)
-    {
-        Intent intent = new Intent(this,change.class);
-        intent.putExtra("change1", change1);
-        intent.putExtra("change2", change2);
-        intent.putExtra("change3", change3);
-//        startActivity(intent);
-        startActivityForResult(intent, 1);
+
+    static class MyHandler extends Handler {
+        WeakReference<show> mActivity;
+
+        MyHandler(show activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            show theActivity = mActivity.get();
+            if(msg.what == 5){
+                //TODO SOMETHING
+                TextView result = theActivity.findViewById(R.id.result);
+                result.setText(msg.obj.toString());
+
+            }
+            super.handleMessage(msg);
+        }
     }
+
 }
